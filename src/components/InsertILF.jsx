cbace540c21f46c9a858c97b8f0411195745353d
matibe95 <matibel2005@gmail.com>
2023-08-04 13:22:31 -0300

    create component to add ilf_point

---
+++ src/components/InsertILF.jsx
@@ -0,0 +1,12 @@
+export const InsertILF = () => {
+  return (
+    <div>
+      <h1></h1>
+      <select name="" id="" className="px-4 py-2 rounded-lg">
+        <option value="baja">Baja</option>
+        <option value="media">Media</option>
+        <option value="alta">Alta</option>
+      </select>
+    </div>
+  );
+};
