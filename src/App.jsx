883561a16d99c8dd01e5f2b39da665ee6a96b1b5
matibe95 <matibel2005@gmail.com>
2023-08-04 13:16:57 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
-import { ValuesTables } from './components/ValuesTables'
+import "./App.css";
+import { ILF } from "./components/ILF";
+import { InsertILF } from "./components/InsertILF";
+import { ValuesTables } from "./components/ValuesTables";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <div>
-      <ValuesTables/>
-    </div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<ValuesTables />} />
+        <Route path="/ilf/add" element={<InsertILF />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
