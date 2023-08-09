b6095359d9a905eb1a00e70cf93096b23f9bb7f8
matibe95 <matibel2005@gmail.com>
2023-08-09 14:41:05 -0300

    create constant to render all the reasons cards

---
+++ src/constants/HomeReasons.jsx
@@ -0,0 +1,24 @@
+import { ClockHeart, Free, Simple } from "../assets/Icons";
+
+export const homeReasons = [
+  {
+    id: "reason-tiempo",
+    title: "Ahorra tiempo",
+    description:
+      "Nuestra web realiza cálculos de puntos de función de forma rápida, pudiendo dedicar más tiempo a otras tareas importantes en tu proyecto",
+    icon: <ClockHeart />,
+  },
+  {
+    id: "reason-simple",
+    title: "Sin complicaciones",
+    description:
+      "La interfaz sencilla y amigable de nuestra web permite a cualquier usuario",
+    icon: <Simple />,
+  },
+  {
+    id: "reason-gratis",
+    title: "Sin costos",
+    description: "Utiliza una herramienta valiosa sin ninguna suscripción.",
+    icon: <Free />,
+  },
+];
