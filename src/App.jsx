c437ee0e72b5229e9ff05b0360f53f05d857277c
matibe95 <matipro365@gmail.com>
2023-08-09 00:15:29 -0300

    add transition all to main div

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { NavBar } from "./components/NavBar";
 function App() {
   return (
     <>
-      <div className="bg-background-fill px-[15%]">
+      <div className="bg-background-fill transition-all duration-700 px-[15%]">
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
