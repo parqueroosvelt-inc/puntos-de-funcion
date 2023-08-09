e7dbaccb6fbb87d36d609fcecd103bb3440f6413
matibe95 <matibel2005@gmail.com>
2023-08-09 14:40:49 -0300

    create constant to use it in footer, contact links

---
+++ src/constants/contactLinks.js
@@ -0,0 +1,17 @@
+export const contactLink = [
+    {
+        id: 'footer-correo',
+        title: 'Correo',
+        link: 'mailto:parqueroosvelt.inc@gmail.com?Subject=Mensaje%20para%20equipo%20de%20Parque%20Roosevelt',
+    },
+    {
+        id: 'footer-instagram',
+        title: 'Instagram',
+        link: 'www.instagram.com/',
+    },
+    {
+        id: 'footer-github',
+        title: 'Github',
+        link: 'https://github.com/parqueroosvelt-inc/puntos-de-funcion',
+    }
+]
