efde9314c557e0f04edd110bb87fb80ed27a6fd2
matibe95 <matibel2005@gmail.com>
2023-08-09 14:39:07 -0300

    set Icons in a single file of components

---
+++ src/assets/Icons.jsx
@@ -0,0 +1,103 @@
+export const ClockHeart = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={74}
+      height={74}
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      className="icon icon-tabler icon-tabler-clock-heart transition-all hover:text-primary"
+      viewBox="0 0 24 24"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M20.956 11.107a9 9 0 1 0-9.579 9.871" />
+      <path d="m18 22 3.35-3.284a2.143 2.143 0 0 0 .005-3.071 2.242 2.242 0 0 0-3.129-.006l-.224.22-.223-.22a2.242 2.242 0 0 0-3.128-.006 2.143 2.143 0 0 0-.006 3.071L18 22zM12 7v5l.5.5" />
+    </svg>
+  );
+};
+
+export const Simple = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={74}
+      height={74}
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      className="icon icon-tabler icon-tabler-heart-handshake transition-all hover:text-primary"
+      viewBox="0 0 24 24"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M19.5 12.572 12 20l-7.5-7.428A5 5 0 1 1 12 6.006a5 5 0 1 1 7.5 6.572" />
+      <path d="M12 6 8.707 9.293a1 1 0 0 0 0 1.414l.543.543c.69.69 1.81.69 2.5 0l1-1a3.182 3.182 0 0 1 4.5 0l2.25 2.25M12.5 15.5l2 2M15 13l2 2" />
+    </svg>
+  );
+};
+
+export const Free = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={74}
+      height={74}
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      className="icon icon-tabler icon-tabler-zoom-money transition-all hover:text-primary"
+      viewBox="0 0 24 24"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M3 10a7 7 0 1 0 14 0 7 7 0 1 0-14 0M21 21l-6-6" />
+      <path d="M12 7H9.5a1.5 1.5 0 0 0 0 3h1a1.5 1.5 0 0 1 0 3H8M10 13v1m0-8v1" />
+    </svg>
+  );
+};
+
+export const MouseIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={44}
+      height={44}
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      className="icon icon-tabler icon-tabler-mouse transition-all duration-700"
+      viewBox="0 0 24 24"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M6 7a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v10a4 4 0 0 1-4 4h-4a4 4 0 0 1-4-4zM12 7v4" />
+    </svg>
+  );
+};
+
+export const PageIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={124}
+      height={124}
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      id="main_icon-code-dots"
+      className="icon icon-tabler icon-tabler-code-dots transition-all duration-700"
+      viewBox="0 0 24 24"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M15 12h.01M12 12h.01M9 12h.01M6 19a2 2 0 0 1-2-2v-4l-1-1 1-1V7a2 2 0 0 1 2-2M18 19a2 2 0 0 0 2-2v-4l1-1-1-1V7a2 2 0 0 0-2-2" />
+    </svg>
+  );
+};
