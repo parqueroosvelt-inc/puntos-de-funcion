423d5b83a484097b658cd222cb20b391b58a54ec
matibe95 <matipro365@gmail.com>
2023-08-09 00:16:16 -0300

    add button to change theme

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,22 @@
 import { NAVBAR_SECTIONS } from "../constants/navbarSections";
+import "@theme-toggles/react/css/Around.css";
+import { Around } from "@theme-toggles/react";
+import "./navBar.css";
+import { useState } from "react";
+import { useTheme } from "../hooks/useChangeTheme";
 
 export const NavBar = () => {
+  // const [theme, setTheme] = useChangeTheme()
+  const { theme, toggleTheme } = useTheme();
+  // const [theme, setTheme] = useState(localStorage.getItem("theme"));
+
+  // const handleTheme = (ev) => {};
+
+  // const changeTheme = (theme) => {
+  //   localStorage.setItem("theme", theme);
+  //   setTheme(theme);
+  // };
+
   return (
     <nav className="text-baseColor py-6">
       <ul className="flex items-center justify-between">
@@ -22,6 +38,10 @@ export const NavBar = () => {
               <path d="M15 12h.01M12 12h.01M9 12h.01M6 19a2 2 0 0 1-2-2v-4l-1-1 1-1V7a2 2 0 0 1 2-2M18 19a2 2 0 0 0 2-2v-4l1-1-1-1V7a2 2 0 0 0-2-2" />
             </svg>
           </a>
+        </li>
+        <li className="flex items-center justify-center">
+          <button onClick={toggleTheme}>Toggle Theme</button>
+          {/* <Around toggled={theme} toggle={setTheme} duration={450} /> */}
         </li>
         {NAVBAR_SECTIONS.map((item) => {
           return (
