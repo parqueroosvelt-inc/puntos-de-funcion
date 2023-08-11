6a7913ab422bbb15f043c1991f5b9979ff74b2b3
matibe95 <matipro365@gmail.com>
2023-08-11 00:56:02 -0300

    add protected route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Home } from "./components/Home";
 import { Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { CalculatePoints } from "./components/CalculatePoints";
+import { VPFA } from "./components/VPFA";
+import { ProtectedRoute } from "./components/utils/ProtectedRoute";
 
 function App() {
   return (
@@ -11,7 +13,14 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/start" element={<CalculatePoints />} />
+          <Route path="/vpfsa" element={<CalculatePoints />} />
+          <Route
+            element={
+              <ProtectedRoute condition={localStorage.getItem("vpfsa-value")} />
+            }
+          >
+            <Route path="/vpfa" element={<VPFA />} />
+          </Route>
         </Routes>
       </div>
     </>
