6dc365ed30333b8dc6ad9e87b7b356932a5923f6
matibe95 <matibel2005@gmail.com>
2023-08-11 13:34:03 -0300

    create route estimacion esfuerzo

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { NavBar } from "./components/NavBar";
 import { CalculatePoints } from "./components/CalculatePoints";
 import { VPFA } from "./components/VPFA";
 import { ProtectedRoute } from "./components/utils/ProtectedRoute";
+import { EstimacionEsfuerzo } from "./components/EstimacionEsfuerzo";
 
 function App() {
   return (
@@ -14,12 +15,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/vpfsa" element={<CalculatePoints />} />
-          <Route
-            element={
-              <ProtectedRoute condition={localStorage.getItem("vpfsa-value")} />
-            }
-          >
+          <Route element={<ProtectedRoute />}>
             <Route path="/vpfa" element={<VPFA />} />
+          </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route
+              path="/estimacion_esfuerzo"
+              element={<EstimacionEsfuerzo />}
+            />
           </Route>
         </Routes>
       </div>
