429bf9ee70a5abebda769963fefca093aa4ad8eb
matibe95 <matipro365@gmail.com>
2023-08-12 19:03:07 -0300

    add dictionary to protected route

--- src/components/utils/ProtectedRoute.jsx
+++ src/components/utils/ProtectedRoute.jsx
@@ -1,7 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-export const ProtectedRoute = ({ redirectPath = "/" }) => {
-  const condition = localStorage.getItem("vpfsa-value");
+const ROUTES_DICTIONARY = {
+  vpfsa: "vpfsa-value",
+  vpfa: "adjusted-influence-value",
+};
+
+export const ProtectedRoute = ({ value, redirectPath = "/" }) => {
+  const condition = localStorage.getItem(ROUTES_DICTIONARY[value]);
   if (condition === null || condition === undefined) {
     return <Navigate to={redirectPath} replace />;
   }
