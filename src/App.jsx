32889a1af8710f9d73c6298a44a498eebf119e6e
matibe95 <matipro365@gmail.com>
2023-08-12 19:00:15 -0300

    add more routes and param to pass

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,27 @@ import { CalculatePoints } from "./components/CalculatePoints";
 import { VPFA } from "./components/VPFA";
 import { ProtectedRoute } from "./components/utils/ProtectedRoute";
 import { EstimacionEsfuerzo } from "./components/EstimacionEsfuerzo";
+import { FinalResults } from "./components/FinalResults";
 
 function App() {
   return (
     <>
-      <div className="container-principal bg-background-fill transition-all duration-700">
+      <div className="container-principal">
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/vpfsa" element={<CalculatePoints />} />
-          <Route element={<ProtectedRoute />}>
+          <Route element={<ProtectedRoute value={"vpfsa"} />}>
             <Route path="/vpfa" element={<VPFA />} />
           </Route>
-          <Route element={<ProtectedRoute />}>
+          <Route element={<ProtectedRoute value={"vpfsa"} />}>
             <Route
               path="/estimacion_esfuerzo"
               element={<EstimacionEsfuerzo />}
             />
+          </Route>
+          <Route element={<ProtectedRoute value={"vpfsa"} />}>
+            <Route path="/results" element={<FinalResults />} />
           </Route>
         </Routes>
       </div>
