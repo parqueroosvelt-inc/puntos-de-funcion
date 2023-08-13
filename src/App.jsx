74ccf3c3bc62b36c304a6d46cc1eadc127095e58
matibe95 <matipro365@gmail.com>
2023-08-13 13:55:15 -0300

    import footer to all routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,26 @@ import { VPFA } from "./components/VPFA";
 import { ProtectedRoute } from "./components/utils/ProtectedRoute";
 import { EstimacionEsfuerzo } from "./components/EstimacionEsfuerzo";
 import { FinalResults } from "./components/FinalResults";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
-      <div className="container-principal">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/vpfsa" element={<CalculatePoints />} />
-          <Route element={<ProtectedRoute value={"vpfsa"} />}>
-            <Route path="/vpfa" element={<VPFA />} />
-          </Route>
-          <Route element={<ProtectedRoute value={"vpfsa"} />}>
-            <Route
-              path="/estimacion_esfuerzo"
-              element={<EstimacionEsfuerzo />}
-            />
-          </Route>
-          <Route element={<ProtectedRoute value={"vpfsa"} />}>
-            <Route path="/results" element={<FinalResults />} />
-          </Route>
-        </Routes>
-      </div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/vpfsa" element={<CalculatePoints />} />
+        <Route element={<ProtectedRoute value={"vpfsa"} />}>
+          <Route path="/vpfa" element={<VPFA />} />
+        </Route>
+        <Route element={<ProtectedRoute value={"vpfsa"} />}>
+          <Route path="/estimacion_esfuerzo" element={<EstimacionEsfuerzo />} />
+        </Route>
+        <Route element={<ProtectedRoute value={"vpfsa"} />}>
+          <Route path="/results" element={<FinalResults />} />
+        </Route>
+      </Routes>
+      <Footer />
     </>
   );
 }
